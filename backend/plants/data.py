109472b1b158a94982e3cca08c72e3f8bb172ee4
tuanasimseek <tuanas5@icloud.com>
2026-02-11 15:33:15 +0300

    resimlerin isimlerini guncelleme

--- backend/plants/data.py
+++ backend/plants/data.py
@@ -10,54 +10,54 @@ PLANTS = [
         "id" : 2,
         "name" : "Maydanoz",
         "size_cm" : 10,
-        "image" : "/static/img/Maydanoz.jpeg",
+        "image" : "/static/img/maydanoz.jpeg",
     },
 
     {
         "id" : 3,
         "name" : "Deve Tabanı",
         "size_cm" : 12,
-        "image" : "/static/img/DeveTabanı.jpeg",
+        "image" : "/static/img/deveTabani.jpeg",
     },
 
     {
         "id" : 4,
         "name" : "Starliçe",
         "size_cm" : 17,
-        "image" : "/static/img/Starliçe.jpeg",
+        "image" : "/static/img/starlice.jpeg",
     },
 
     {
         "id" : 5,
         "name" : "Şeflera",
         "size_cm" : 20,
-        "image" : "/static/img/Şeflera.jpeg",
+        "image" : "/static/img/seflera.jpeg",
     },
     {
         "id" : 6,
         "name" : "Kauçuk",
         "size_cm" : 14,
-        "image" : "/static/img/Kauçuk.jpeg",
+        "image" : "/static/img/kaucuk.jpeg",
     },
 
     {
         "id" : 7,
         "name" : "Pathos Sarmaşığı",
         "size_cm" : 11,
-        "image" : "/static/img/PathosSarmaşığı.jpeg",
+        "image" : "/static/img/pathosSarmasigi.jpeg",
     },
     {
         "id" : 8,
         "name" : "Spatifilyum",
         "size_cm" : 9,
-        "image" : "/static/img/Spatifilyum.jpeg",
+        "image" : "/static/img/spatifilyum.jpeg",
     },
 
     {
         "id" : 9,
         "name" : "Antoryum",
         "size_cm" : 9,
-        "image" : "/static/img/Antoryum.jpeg",
+        "image" : "/static/img/antoryum.jpeg",
     },
 
     {
@@ -71,70 +71,70 @@ PLANTS = [
         "id" : 11,
         "name" : "Lirata",
         "size_cm" : 22,
-        "image" : "/static/img/Lirata.jpeg",
+        "image" : "/static/img/lirata.jpeg",
     },
 
     {
         "id" : 12,
         "name" : "Siklamen",
         "size_cm" : 5,
-        "image" : "/static/img/Siklamen.jpeg",
+        "image" : "/static/img/siklamen.jpeg",
     },
 
     {
         "id" : 13,
-        "name" : "Kalanchoe",
+        "name" : "alanchoe",
         "size_cm" : 13,
-        "image" : "/static/img/Kalanchoe.jpeg",
+        "image" : "/static/img/kalanchoe.jpeg",
     },
 
     {
         "id" : 14,
         "name" : "Sardunya",
         "size_cm" : 17,
-        "image" : "/static/img/Sardunya.jpeg",
+        "image" : "/static/img/sardunya.jpeg",
     },
 
     {
         "id" : 15,
         "name" : "Draçena",
         "size_cm" : 30,
-        "image" : "/static/img/Draçena.jpeg",
+        "image" : "/static/img/dracena.jpeg",
     },
 
     {
         "id" : 16,
         "name" : "Marginata",
         "size_cm" : 36,
-        "image" : "/static/img/Marginata.jpeg",
+        "image" : "/static/img/marginata.jpeg",
     },
 
     {
         "id" : 17,
         "name" : "Marul",
         "size_cm" : 10,
-        "image" : "/static/img/Marul.jpeg",
+        "image" : "/static/img/marul.jpeg",
     },
 
     {
         "id" : 18,
         "name" : "Pilea",
         "size_cm" : 10,
-        "image" : "/static/img/Pilea.jpeg" ,
+        "image" : "/static/img/pilea.jpeg" ,
     },
 
     {
         "id" : 19,
         "name" : "Süt kaktusu",
         "size_cm" : 27,
-        "image" : "/static/img/SütKaktusu.jpeg",
+        "image" : "/static/img/sutKaktusu.jpeg",
     },
     
     {
         "id" : 20,
         "name" : "Yuka",
         "size_cm" : 33,
-        "image" :"/static/img/Yuka.jpeg" ,
+        "image" :"/static/img/yuka.jpeg" ,
     },
 
 ]
